c40e703f02f83f64bd72f83f6715ae2baf77bf91
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-05 22:41:33 -0600

    Update name and instructor greeting2.py

--- name and instructor greeting2.py
+++ name and instructor greeting2.py
@@ -4,10 +4,19 @@
 #
 #This program requests input of my name
 #and the first name of my instructor for the CSS225 class
-
+#This is the correct version and return the expected result.  You did well!
 username = input("Please enter your name: >> ")
 #This line accepts input of a name to verify the following validated names
-
+#You don't need to define two different variable names here.  Just one NAME would be sufficient.  See the sample code below:
+#name = str(input("Please enter your name:"))
+#if name == "Student1": # Add your name
+#	print("Hello,",name,"!")
+	
+#elif name == "Laleh": #Add instuctor's name
+#                print("Hello,",name,"!")
+	
+#else:
+#	print("Name not recognized.")
 user1 = "David"
 user2 = "Laleh"
 #These are the only names that the program will accept and respond to
