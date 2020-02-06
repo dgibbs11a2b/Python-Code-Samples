e036c18131382397e5af1ed0dd615bc41c92550f
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-05 22:58:13 -0600

    Update starting day number.py

--- starting day number.py
+++ starting day number.py
@@ -2,3 +2,5 @@ start_day = int(input("Ranging from 0 to 6 days with 0 representing Sunday, from
 days_to_wait = int(input("And what is the length of your stay? "))
 end_day = (start_day + days_to_wait) % 7
 print(end_day)
+#While your program works but it doesn't return the weekday name.  
+#This one wasn't one of my favorite first-time coding assignments but there was more to it than what you did.  
